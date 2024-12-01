2dd056db130892c6e42d56e0245e9c4e4b5a0b7d
Inscode <inshafahmedh@gmail.com>
2024-12-01 22:38:28 +0530

    complete the project

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ListProduct.css";
+import cross_icon from "../../assets/cancel.png";
 
 const ListProduct = () => {
   const [allproducts, setAllProducts] = useState([]);
@@ -11,6 +12,23 @@ const ListProduct = () => {
         setAllProducts(data);
       });
   };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  const remove_product = async (id) => {
+    await fetch("http://localhost:4000/removeproduct", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: id }),
+    });
+    await fetchInfo();
+  };
+
   return (
     <div className="list-product">
       <h1>All Product List</h1>
@@ -22,7 +40,38 @@ const ListProduct = () => {
         <p>Category</p>
         <p>Remove</p>
       </div>
-      <div className="listproduct-allproducts"></div>
+      <div className="listproduct-allproducts">
+        <hr />
+        {allproducts.map((product, index) => {
+          return (
+            <>
+              <div
+                key={index}
+                className="listproduct-format-main listproduct-format"
+              >
+                <img
+                  src={product.image}
+                  alt=""
+                  className="listproduct-product-icon"
+                ></img>
+                <p>{product.name}</p>
+                <p>Rs {product.old_price}</p>
+                <p>Rs {product.new_price}</p>
+                <p>{product.category}</p>
+                <img
+                  onClick={() => {
+                    remove_product(product.id);
+                  }}
+                  className="listproduct-remove-icon"
+                  src={cross_icon}
+                  alt=""
+                />
+              </div>
+              <hr />
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 };
